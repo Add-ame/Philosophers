d6efc421f39fe2da73102137c9ddc5d24549f391
Add-ame <simoaddame17@gmail.com>
2025-02-17 16:26:31 +0100

    Philo

--- philo.c
+++ philo.c
@@ -1,51 +1,32 @@
 #include "philo.h"
 
-int		balance = 0;
-
-int		read_balance()
+void	*add(void *i)
 {
-	usleep(250000);
-	return (balance);
-}
-
-void	write_balance(int new_balance)
-{
-	usleep(250000);
-	balance = new_balance;
-}
-
-pthread_mutex_t	mutex;
-
-void	*deposit(void *amount)
-{
-	int		acc_amount;
-
-	pthread_mutex_lock(&mutex);
-	acc_amount = read_balance();
-	acc_amount += *((int *)amount);
-	write_balance(acc_amount);
-	pthread_mutex_unlock(&mutex);
+	printf("%d thinking\n", *((int *)i));
 
 	return NULL;
 }
 
 int		main()
 {
-	printf("before: %d\n", read_balance());
+	pthread_t	t[5000];
+	int		i;
 
-	pthread_t	t1;
-	pthread_t	t2;
+	i = 0;
+	while (i <= 5000)
+	{
+		if (pthread_create(&t[i], NULL, add, (void *)&i) != 0)
+			return (perror("fail"), 1);
+		i++;
+	}
+	i = 0;
+	while (i <= 5000)
+	{
+		if (pthread_join(t[i], NULL) != 0)
+			return (perror("fail"), 1);
+		i++;
+	}
+	printf("died\n");
 
-	int		v1 = 300;
-	int		v2 = 200;
-
-	pthread_mutex_init(&mutex, NULL);
-	pthread_create(&t1, NULL, deposit, (void *) &v1);
-	pthread_create(&t2, NULL, deposit, (void *) &v2);
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
-
-	pthread_mutex_destroy(&mutex);
-	printf("after: %d\n", read_balance());
 	return (0);
 }
