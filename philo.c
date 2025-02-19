95ba1d15839a502567238902b36b845a8294427a
Add-ame <simoaddame17@gmail.com>
2025-02-19 09:47:41 +0100

    think -> die

--- philo.c
+++ philo.c
@@ -1,31 +1,40 @@
 #include "philo.h"
 
+pthread_mutex_t		mutex;
+
 void	*add(void *i)
 {
+	pthread_mutex_lock(&mutex);
 	printf("%d thinking\n", *((int *)i));
 
+	pthread_mutex_unlock(&mutex);
 	return NULL;
 }
 
 int		main()
 {
-	pthread_t	t[5000];
+	pthread_t	t[4];
 	int		i;
+	int		*a;
 
+	pthread_mutex_init(&mutex, NULL);
 	i = 0;
-	while (i <= 5000)
+	while (i < 3)
 	{
-		if (pthread_create(&t[i], NULL, add, (void *)&i) != 0)
+		a = malloc(sizeof(int));
+		*a = i;
+		if (pthread_create(&t[i], NULL, add, a) != 0)
 			return (perror("fail"), 1);
 		i++;
 	}
 	i = 0;
-	while (i <= 5000)
+	while (i < 3)
 	{
 		if (pthread_join(t[i], NULL) != 0)
 			return (perror("fail"), 1);
 		i++;
 	}
+	pthread_mutex_destroy(&mutex);
 	printf("died\n");
 
 	return (0);
