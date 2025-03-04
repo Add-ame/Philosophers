f4cffa3b163cc9091720b86af3e475578a7eac4f
Add-ame <simoaddame17@gmail.com>
2025-03-04 16:28:55 +0000

    eat your meals fixed

--- init_philo_data.c
+++ init_philo_data.c
@@ -1,13 +1,13 @@
 #include "philo.h"
 
-void	init(t_philo *p, int i, pthread_mutex_t *mutex, t_plate *pl, pthread_mutex_t *up)
+void	init(t_philo *p, int i, t_plate *pl)
 {
 	p->idx = i + 1;
 	p->meals_eaten = 0;
-	p->print = mutex;
-	p->start_time = get_real_time();
-	p->last_meal_time = get_real_time();
+	p->start_time = get_real_time(p, 0);
+	p->last_meal_time = get_real_time(p, 0);
 	p->die = 0;
 	p->plate = pl;
-	p->upd = up;
+	p->print = &pl->mutex;
+	p->upd = &pl->mutex_upd;
 }
