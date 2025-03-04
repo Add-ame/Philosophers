cdc4300c668a09929ed044373d1b041f2cea4605
Add-ame <simoaddame17@gmail.com>
2025-03-04 12:05:31 +0000

    Finaly Data Races Fixed

--- init_philo_data.c
+++ init_philo_data.c
@@ -1,6 +1,6 @@
 #include "philo.h"
 
-void	init(t_philo *p, int i, pthread_mutex_t *mutex, t_plate *pl)
+void	init(t_philo *p, int i, pthread_mutex_t *mutex, t_plate *pl, pthread_mutex_t *up)
 {
 	p->idx = i + 1;
 	p->meals_eaten = 0;
@@ -9,4 +9,5 @@ void	init(t_philo *p, int i, pthread_mutex_t *mutex, t_plate *pl)
 	p->last_meal_time = get_real_time();
 	p->die = 0;
 	p->plate = pl;
+	p->upd = up;
 }
