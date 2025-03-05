1400749a1464df54ade5aece90e7842bb95a855d
Add-ame <simoaddame17@gmail.com>
2025-03-05 21:34:28 +0000

    ...

--- philo.h
+++ philo.h
@@ -12,15 +12,16 @@
 # define CHECK_STARVED 1
 # define CHECK_FULL 2
 # define ERROR -1
+# define END 1
 
-typedef enum {
-	def,
-	picked,
-	throwed,
-	eating,
-	thinking,
-	sleeping
-}	t_enum;
+// typedef enum {
+// 	def,
+// 	picked,
+// 	throwed,
+// 	eating,
+// 	thinking,
+// 	sleeping
+// }	t_enum;
 
 
 typedef	struct s_data
@@ -31,8 +32,10 @@ typedef	struct s_data
 	int		time_to_sleep;
 	long	simulation_end_time;
 	int		must_eat_num;
+	int		died;
+	int		all_eaten;
 	pthread_mutex_t mutex;
-	pthread_mutex_t mutex_upd;
+	pthread_mutex_t checking;
 }	t_plate;
 
 typedef struct s_philo
@@ -46,22 +49,35 @@ typedef struct s_philo
 	t_plate		*plate;
 	pthread_t	thread;
 	pthread_mutex_t	*print;
-	pthread_mutex_t	*upd;
+	pthread_mutex_t	*checking;
 	pthread_mutex_t	left_fork;
 	pthread_mutex_t	*right_fork;
 }   t_philo;
 
 
-/*                    init_table                    */
+void	*add(void *data);
+
+/*                    init                    */
 void    init_table(t_plate *table, int ac, char **av);
-
-/*                     died                         */
-int		died(t_philo *p);
-
-/*                     died                         */
 void	init(t_philo *p, int i, t_plate *pl);
+int		init_mutexes(t_philo *p, t_plate *table);
 
 /*                     time                         */
 long	get_real_time(t_philo *p, int flag);
+
+/*                     clean                         */
+int		clean(t_philo *p, t_plate *table);
+
+/*                     check_philo                         */
+int		check_philo(t_philo *p, int flag);
+
+/*                     check_philo                         */
+void	*one_thread(void	*data);
+int		one_philo(t_philo *p, t_plate *table);
+
+/*                     create_threads                         */
+int		create_threads(t_philo *p, t_plate *table);
+
+int		_printf(t_philo *p, char *s);
 
 #endif
