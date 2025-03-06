4c7af486be81b4a43060a965fd16495704679938
Add-ame <simoaddame17@gmail.com>
2025-03-06 16:28:59 +0000

    Mandatory Done

--- check_philo.c
+++ check_philo.c
@@ -1,30 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_philo.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maddame <maddame@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 15:52:21 by maddame           #+#    #+#             */
+/*   Updated: 2025/03/06 15:52:23 by maddame          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-int		check_philo(t_philo *p, int flag)
+int	check_philo(t_philo *p, int flag)
 {
 	if (flag == CHECK_STARVED)
 	{
-		if (get_real_time(p, 2) >= p->plate->time_to_die)
+		if (get_time(p, diff_last_to_new) >= p->table->time_to_die)
 		{
 			pthread_mutex_lock(p->print);
-			p->plate->died++;
+			p->table->died++;
 			pthread_mutex_unlock(p->print);
-			p->die = STARVED;
 			pthread_mutex_lock(p->checking);
-			p->plate->simulation_end_time = get_real_time(p, 1);
+			p->table->simulation_end_time = get_time(p, diff_start_to_new);
 			pthread_mutex_unlock(p->checking);
 			pthread_mutex_lock(p->print);
-			if (p->plate->died == 1)
-				printf("%ld %d died\n", p->plate->simulation_end_time, p->idx);
+			if (p->table->died == 1)
+				printf("%ld %d died\n", p->table->simulation_end_time, p->idx);
 			pthread_mutex_unlock(p->print);
 			return (STARVED);
 		}
 	}
 	else if (flag == CHECK_FULL)
 	{
-		if (p->meals_eaten == p->plate->must_eat_num)
+		if (p->meals_eaten == p->table->num_to_eat)
 		{
-			p->die = FULL;
 			return (FULL);
 		}
 	}
