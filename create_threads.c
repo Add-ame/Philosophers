6eb70cc48bb8604273ebf623f6290eda42528a83
Add-ame <simoaddame17@gmail.com>
2025-03-06 16:29:37 +0000

    Mandatory Done

--- create_threads.c
+++ create_threads.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_threads.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maddame <maddame@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 15:52:27 by maddame           #+#    #+#             */
+/*   Updated: 2025/03/06 15:52:28 by maddame          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-int		create_threads(t_philo *p, t_plate *table)
+int	create_threads(t_philo *p, t_table *table)
 {
 	int		i;
 
@@ -9,16 +21,16 @@ int		create_threads(t_philo *p, t_plate *table)
 	{
 		init(&p[i], i, table);
 		p[i].right_fork = &p[(i + 1) % table->num_philos].left_fork;
-		if (pthread_create(&p[i].thread, NULL, add, &p[i]) != 0)
-			return (perror("fail"), ERROR);
+		if (pthread_create(&p[i].thread, NULL, add, &p[i]) != SUCESS)
+			return (ERROR);
 		i++;
 	}
 	i = 0;
 	while (i < table->num_philos)
 	{
-		if (pthread_join(p[i].thread, NULL) != 0)
-			return (perror("fail"), ERROR);
+		if (pthread_join(p[i].thread, NULL) != SUCESS)
+			return (ERROR);
 		i++;
 	}
-	return (0);
+	return (SUCESS);
 }
