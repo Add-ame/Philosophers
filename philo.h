b78a530a8943a8df18d36b5ce0a902dd646fba69
Add-ame <simoaddame17@gmail.com>
2025-03-06 16:30:00 +0000

    Mandatory Done

--- philo.h
+++ philo.h
@@ -1,11 +1,23 @@
-#ifndef PHILO_H
-#define PHILO_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maddame <maddame@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 15:52:59 by maddame           #+#    #+#             */
+/*   Updated: 2025/03/06 16:06:41 by maddame          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/time.h>
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
 
 # define STARVED 1
 # define FULL 2
@@ -13,71 +25,77 @@
 # define CHECK_FULL 2
 # define ERROR -1
 # define END 1
+# define INV_ARG 22
+# define SUCESS 0
+# define FAIL 1
 
-// typedef enum {
-// 	def,
-// 	picked,
-// 	throwed,
-// 	eating,
-// 	thinking,
-// 	sleeping
-// }	t_enum;
+typedef pthread_mutex_t	t_mutex;
 
-
-typedef	struct s_data
+typedef enum s_enum
 {
-	int		num_philos;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	long	simulation_end_time;
-	int		must_eat_num;
-	int		died;
-	int		all_eaten;
-	pthread_mutex_t mutex;
-	pthread_mutex_t checking;
-}	t_plate;
+	even_number,
+	left_then_right,
+	right_then_left,
+	current_time,
+	diff_start_to_new,
+	diff_last_to_new,
+	first_fork,
+	both_forks
+}	t_enum;
+
+typedef struct s_table
+{
+	long		time_to_die;
+	long		time_to_eat;
+	long		time_to_sleep;
+	long		simulation_end_time;
+	long		num_to_eat;
+	int			num_philos;
+	int			died;
+	int			all_eaten;
+	t_mutex		mutex;
+	t_mutex		checking;
+}	t_table;
 
 typedef struct s_philo
 {
-	long	start_time;
-	int		die;
-	int		idx;
-	int		real_time;
-	int		meals_eaten;
-	long	last_meal_time;
-	t_plate		*plate;
+	long		start_time;
+	int			idx;
+	int			real_time;
+	int			meals_eaten;
+	long		last_meal_time;
+	t_table		*table;
 	pthread_t	thread;
-	pthread_mutex_t	*print;
-	pthread_mutex_t	*checking;
-	pthread_mutex_t	left_fork;
-	pthread_mutex_t	*right_fork;
-}   t_philo;
-
+	t_mutex		*print;
+	t_mutex		*checking;
+	t_mutex		left_fork;
+	t_mutex		*right_fork;
+}	t_philo;
 
 void	*add(void *data);
 
 /*                    init                    */
-void    init_table(t_plate *table, int ac, char **av);
-void	init(t_philo *p, int i, t_plate *pl);
-int		init_mutexes(t_philo *p, t_plate *table);
-
-/*                     time                         */
-long	get_real_time(t_philo *p, int flag);
+int		init_table(t_table *table, int ac, char **av);
+void	init(t_philo *p, int i, t_table *pl);
+int		init_mutexes(t_philo *p, t_table *table);
 
 /*                     clean                         */
-int		clean(t_philo *p, t_plate *table);
+int		clean(t_philo *p, t_table *table);
 
 /*                     check_philo                         */
 int		check_philo(t_philo *p, int flag);
 
 /*                     check_philo                         */
 void	*one_thread(void	*data);
-int		one_philo(t_philo *p, t_plate *table);
+int		one_philo(t_table *table);
 
 /*                     create_threads                         */
-int		create_threads(t_philo *p, t_plate *table);
+int		create_threads(t_philo *p, t_table *table);
 
-int		_printf(t_philo *p, char *s);
+/*                    philo_utils                  */
+long	get_time(t_philo *p, int flag);
+int		ft_print(t_philo *p, char *s, int flag);
+void	lock_fork(t_philo *p, int flag);
+void	unlock_fork(t_philo *p, int flag);
 
 #endif
